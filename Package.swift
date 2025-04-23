73150927ca69bfd79aadc55ea60776dd59fc156f
Vyacheslav <vyacheslav.razumeenko@gmail.com>
2025-04-23 20:01:42 +0300

    feature: updated package OS version support

--- Package.swift
+++ Package.swift
@@ -1,31 +1,27 @@
-// swift-tools-version: 5.5
+// swift-tools-version: 6.0
 
 /**
-*  SwiftUISnappingScrollView
-*  Copyright (c) Ciaran O'Brien 2022
-*  MIT license, see LICENSE file for details
-*/
+ *  SwiftUISnappingScrollView
+ *  Copyright (c) Ciaran O'Brien 2022
+ *  MIT license, see LICENSE file for details
+ */
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftUISnappingScrollView",
-    platforms: [
-        .iOS(.v13)
-//        .macCatalyst(.v14),
-//        .macOS(.v11),
-//        .tvOS(.v14),
-//        .watchOS(.v7)
-    ],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "SwiftUISnappingScrollView",
-            targets: ["SwiftUISnappingScrollView"]),
+            targets: ["SwiftUISnappingScrollView"]
+        ),
     ],
     dependencies: [],
     targets: [
         .target(
             name: "SwiftUISnappingScrollView",
-            dependencies: [])
+            dependencies: []
+        )
     ]
 )
